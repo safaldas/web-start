e2ed5be8badb880522481575a872e16dfd3b4ee0
safaldas <safaldasg@gmail.com>
2016-11-15 17:38:06 +0530

    changed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 const gulp = require('gulp'),
   del = require('del'),
   less = require('gulp-less'),
-  cssmin = require('gulp-minify-css'),
+  postcss = require('gulp-postcss'),
+  cssnext = require('postcss-cssnext'),
+  cssnano = require('cssnano'),
+  cssmin = require('gulp-clean-css'),
   concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  plumber = require('gulp-plumber'),
+  cachebust = require('gulp-cache-bust'),
   webpack = require('webpack-stream'),
-  Karma = require('karma');
+  browserSync = require('browser-sync'),
+  reload = browserSync.reload,
+  stream = browserSync.stream;
 
 const config = {
   paths: {
+    vendor: {
+      js: [
+      ],
+      css: [
+      ]
+    },
     css: [
       './src/css/**/*.css'
     ],
@@ -17,16 +31,30 @@ const config = {
     js: [
       './src/**/*.js'
     ],
+    sw: [
+      './src/**/*-sw.js'
+    ],
     app: './src/app.js',
-    html: ['./src/**/*.html'],
+    html: ['./src/*.html'],
+    fonts: [
+    ],
     images: './src/images/**/*.*',
   },
   dest: {
-    style: 'style.css',
+    static: {
+      css: 'dist/static/css',
+      js: 'dist/static/js',
+      sw: 'dist/static/js/sw',
+      images: 'dist/static/images',
+      fonts: 'dist/static/fonts',
+      sw:'dist/static/js/sw',
+    },
+    vendor_css: 'vendor.css',
+    vendor_js: 'vendor.js',
+    app_css: 'style.css',
     app: 'dist',
     dist: 'dist',
     less: 'src/css',
-    images: 'dist/images',
   }
 };
 
@@ -36,104 +64,153 @@ const config = {
  */
 
 gulp.task('clean', function (cb) {
-  del([config.dest.panel.dist + '/**'], cb).then((paths) => {
+  del([config.dest.dist + '/**'], cb).then((paths) => {
     console.log('Deleted files and folders:\n', paths.join('\n'))
   });
-})
+});
 
-  /**
-   * Copy html files from src to dist
-   */
-  .task('html', function () {
-    return gulp.src(config.paths.html)
-      .pipe(gulp.dest(config.dest.dist))
-  })
+// run browser sync server
+gulp.task('server', function () {
+  browserSync({
+    server: {
+      baseDir: config.dest.dist,
+      index: 'index.html',
+      routes: {
+      }
+    }
+  });
+});
 
-  .task('images', function () {
-    return gulp.src(config.paths.images)
-      .pipe(gulp.dest(config.dest.images))
-  })
+gulp.task('login', function () {
+  browserSync({
+    server: {
+      baseDir: config.dest.dist,
+      index: 'login.html'
+    }
+  });
+});
 
-  .task('less', function () {
-    return gulp.src(config.paths.less)
-      .pipe(less())
-      .pipe(gulp.dest(config.dest.less))
-  })
+gulp.task('vendor:css', function () {
+  return gulp.src(config.paths.vendor.css)
+    .pipe(concat(config.dest.vendor_css))
+    .pipe(postcss([
+      cssnano({
+        discardComments: {
+          removeAll: true
+        }
+      })
+    ]))
+    .pipe(gulp.dest(config.dest.static.css))
+    .pipe(stream());
+});
 
-  .task('css', ['less'], function () {
-    return gulp.src(config.paths.css)
-      .pipe(concat(config.dest.style))
-      .pipe(gulp.dest(config.dest.dist))
-  })
+gulp.task('vendor:js', function () {
+  return gulp.src(config.paths.vendor.js)
+    .pipe(concat(config.dest.vendor_js))
+    .pipe(uglify())
+    .pipe(gulp.dest(config.dest.static.js))
+    .pipe(stream());
+});
 
-  .task('css:min', ['less'], function () {
-    return gulp.src(config.paths.css)
-      .pipe(concat(config.dest.style))
-      .pipe(cssmin())
-      .pipe(gulp.dest(config.dest.dist))
-  })
+gulp.task('html', function () {
+  return gulp.src(config.paths.html)
+    .pipe(cachebust({
+      type: 'timestamp'
+    }))
+    .pipe(gulp.dest(config.dest.dist))
+});
 
-  .task('js', function () {
-    return gulp.src(config.paths.app)
-      .pipe(webpack(require('./webpack.config.dev.js')))
-      .pipe(gulp.dest(config.dest.app))
-  })
-  .task('js:min', function () {
-    return gulp.src(config.paths.js)
-      .pipe(webpack(require('./webpack.config.prod.js')))
-      .pipe(gulp.dest(config.dest.app))
-  })
+gulp.task('images', function () {
+  return gulp.src(config.paths.images)
+    .pipe(gulp.dest(config.dest.static.images))
+});
 
-  // Watches
-  .task('watch:html', function () {
-    return gulp.watch(
-      [
-        config.paths.html
-      ], [
-        'html'
-      ]);
-  })
-  .task('watch:images', function () {
-    return gulp.watch(
-      [
-        config.paths.images
-      ], [
-        'images'
-      ]);
-  })
-  .task('watch:less', function () {
-    return gulp.watch(
-      [
-        config.paths.less
-      ], [
-        'css'
-      ]);
-  })
+gulp.task('sw', function () {
+  return gulp.src(config.paths.sw)
+    .pipe(gulp.dest(config.dest.static.sw))
+});
 
-  .task('watch', ['watch:html', 'watch:images', 'watch:less'])
+gulp.task('fonts', function () {
+  return gulp.src(config.paths.fonts)
+    .pipe(gulp.dest(config.dest.static.fonts))
+});
 
-  /********************
-   * *               **
-   * *      TEST     **
-   * *               **
-   ********************/
+gulp.task('less', function () {
+  return gulp.src(config.paths.less)
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(gulp.dest(config.dest.less))
+});
 
-  .task('test', function (done) {
-    return new Karma.Server({
-      configFile: __dirname + '/karma.conf.js',
-      singleRun: false
-    }, done).start();
-  })
+gulp.task('css', ['less'], function () {
+  return gulp.src(config.paths.css)
+    .pipe(concat(config.dest.app_css))
+    .pipe(postcss([require('autoprefixer')]))
+    .pipe(gulp.dest(config.dest.static.css))
+});
 
-  .task('test:1', function (done) {
-    return new Karma.Server({
-      configFile: __dirname + '/karma.conf.js',
-      singleRun: true
-    }, done).start();
-  })
+gulp.task('css:min', ['less'], function () {
+  return gulp.src(config.paths.css)
+    .pipe(concat(config.dest.style))
+    .pipe(postcss([
+      cssnano({
+        discardComments: {
+          removeAll: true
+        }
+      })
+    ]))
+    .pipe(gulp.dest(config.dest.dist))
+});
 
-  .task('run', ['html', 'images', 'css', 'js', 'watch'])
-  .task('build', ['html', 'images', 'css', 'js'])
-  .task('build:min', ['html', 'images', 'css:min', 'js:min'])
+gulp.task('js', ['sw'], function () {
+  return gulp.src(config.paths.app)
+    .pipe(webpack(require('./webpack.config.dev.js')))
+    .pipe(gulp.dest(config.dest.static.js))
+});
+gulp.task('js:min', function () {
+  return gulp.src(config.paths.js)
+    .pipe(webpack(require('./webpack.config.prod.js')))
+    .pipe(gulp.dest(config.dest.static.js))
+});
 
-  .task('default', ['run']);
+// Watches
+gulp.task('watch:html', function () {
+  return gulp.watch(
+    [
+      config.paths.html
+    ], [
+      'html', reload
+    ]);
+});
+gulp.task('watch:images', function () {
+  return gulp.watch(
+    [
+      config.paths.images
+    ], [
+      'images', reload
+    ]);
+});
+gulp.task('watch:less', function () {
+  return gulp.watch(
+    [
+      config.paths.less
+    ], [
+      'css', reload
+    ]);
+});
+gulp.task('watch:sw', function () {
+  return gulp.watch(
+    [
+      config.paths.sw
+    ], [
+      'sw'
+    ]);
+});
+
+gulp.task('watch', ['watch:html', 'watch:images', 'watch:less', 'watch:sw']);
+
+gulp.task('run', ['html', 'images', 'css', 'js', 'fonts', 'vendor:js', 'vendor:css', 'watch', 'sw','server']);
+gulp.task('build', ['html', 'images', 'css', 'fonts', 'vendor:js', 'vendor:css', 'js']);
+gulp.task('build:min', ['html', 'images', 'css:min', 'js:min']);
+
+gulp.task('default', ['run']);
